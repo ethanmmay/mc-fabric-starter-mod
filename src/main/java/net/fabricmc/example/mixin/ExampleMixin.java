48ce568659bbbecd4dc68a9b6554b270fb62013e
Ethan May <ethaniel.may@gmail.com>
2022-06-23 18:54:34 -0600

    Dependency Error: Moving to Updated Example Mod

--- src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
@@ -1,5 +1,6 @@
-package starter.mixin;
+package net.fabricmc.example.mixin;
 
+import net.fabricmc.example.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -7,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class StarterMixin {
-  @Inject(at = @At("HEAD"), method = "init()V")
-  private void init(CallbackInfo info) {
-    System.out.println("This line is printed by an example mod mixin!");
-  }
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
 }
